d82c9e168de96093cbd7ce20ab8d4f72af1f9900
Kosta-111 <nestt@ukr.net>
2024-02-10 21:39:45 +0200

    create Note class, edit Products

---
+++ CalorieTracker/Note.cs
@@ -0,0 +1,16 @@
+﻿namespace CalorieTracker;
+
+class Note
+{
+    public DateTime DateTime { get; set; } = DateTime.Now;
+
+    public Product Product { get; set; }
+
+    public double Weight { get; set; }
+
+    public double Proteins => Weight * Product.proteins / 100;
+    public double Fats => Weight * Product.fats / 100;
+    public double Carbohydrates => Weight * Product.carbohydrates / 100;
+    public double Calories => Weight * Product.calories / 100;
+}
+
---
+++ CalorieTracker/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieTracker
+{
+    internal class Product
+    {
+        enum Category
+        {
+            Vegetables,
+            Fruit,
+            Grains,
+            Meat,
+            Seafood,
+            Dairy,
+            Eggs
+        }
+
+        public string name;
+        public double proteins;
+        public double fats;
+        public double carbohydrates;
+        public double calories;
+
+        public Product()
+        {
+            name = "unknown";
+            proteins = 0;
+            fats = 0;
+            carbohydrates = 0;
+            calories = 0;
+        }
+
+        public Product(string name, double proteins, double fats, double carbohydrates, double calories)
+        {
+            this.name = name;
+            this.proteins = proteins;
+            this.fats = fats;
+            this.carbohydrates = carbohydrates;
+            this.calories = calories;
+        }
+
+        
+    }
+}
