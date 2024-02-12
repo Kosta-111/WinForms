2c5589ac87d97150438eff6333ef724a20e7fb30
Voinore1 <vovavoino3@gmail.com>
2024-02-12 23:40:11 +0200

    Some updates(just created form and link winth main form)

--- CalorieTracker/Form1.cs
+++ CalorieTracker/Form1.cs
@@ -2,9 +2,17 @@ namespace CalorieTracker
 {
     public partial class Form1 : Form
     {
+        List<Product> products = new List<Product>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void editing_btn_Click(object sender, EventArgs e)
+        {
+            FORMEditingAdding form = new FORMEditingAdding();
+            form.ShowDialog();
         }
     }
 }
