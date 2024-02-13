ddcbcc7dd2be3366b5dbd97e04ae6a936a201649
Voinore1 <vovavoino3@gmail.com>
2024-02-13 21:04:14 +0200

    Created a list with functions(serialize and deserialize) for it

--- CalorieTracker/FORMEditingAdding.cs
+++ CalorieTracker/FORMEditingAdding.cs
@@ -12,13 +12,52 @@ namespace CalorieTracker
 {
     public partial class FORMEditingAdding : Form
     {
-        List<Product> products = new List<Product>();
+        public Product p { get; set; }
+        public bool changes { get; set; }
+        public BindingList<Product> products { get; set; }
 
-        public FORMEditingAdding()
+        public FORMEditingAdding(BindingList<Product> product)
         {
             InitializeComponent();
+            this.products = product;
+            product_lb.DataSource = this.products;
+            Catgr_cb.DataSource = Enum.GetValues(typeof(Category));
+            changes = false;
         }
 
+        private void Add_Btn_Click(object sender, EventArgs e)
+        {
+            p = new Product()
+            {
+                Name = name_tb.Text,
+                Calories = double.Parse(Calo_tb.Text),
+                Carbohydrates = double.Parse(Carb_tb.Text),
+                Fats = double.Parse(fat_tb.Text),
+                Proteins = double.Parse(Prot_tb.Text),
+                ProductCategory = (Category)Catgr_cb.SelectedItem
+            };
 
+            changes = true;
+
+            this.products.Add(p);
+        }
+
+        private void Cnl_Btn_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void dlt_btn_Click(object sender, EventArgs e)
+        {
+            if (product_lb != null)
+            {
+                Product p = (Product)product_lb.SelectedItem;
+                products.Remove(p);
+                changes = true;
+
+            }
+
+        }
     }
 }
--- CalorieTracker/Form1.cs
+++ CalorieTracker/Form1.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel;
+using System.Text.Json;
+
 namespace CalorieTracker
 {
     public partial class Form1 : Form
     {
-        List<Product> products = new List<Product>();
+        BindingList<Product> products = new BindingList<Product>();
 
         public Form1()
         {
@@ -11,8 +14,28 @@ namespace CalorieTracker
 
         private void editing_btn_Click(object sender, EventArgs e)
         {
-            FORMEditingAdding form = new FORMEditingAdding();
-            form.ShowDialog();
+            string fileName = "product_list.json";
+            string jsonStr;
+
+            if (!products.Any() && File.Exists(fileName))
+            {
+                jsonStr = File.ReadAllText(fileName);
+                products = JsonSerializer.Deserialize<BindingList<Product>>(jsonStr);
+            }
+            else using (File.Create(fileName)) ;
+
+
+            FORMEditingAdding form = new FORMEditingAdding(products);
+            var result = form.ShowDialog();
+
+            if (result == DialogResult.Cancel && form.changes == true)
+            {
+                products = form.products;
+                jsonStr = JsonSerializer.Serialize(products);
+                File.WriteAllText(fileName, jsonStr);
+            }
+
         }
+
     }
 }
